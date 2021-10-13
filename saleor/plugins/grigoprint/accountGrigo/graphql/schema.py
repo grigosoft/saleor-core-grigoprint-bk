17d3827c4b8d658a26bb0b82fb792495ca687f3b
Antonio <antonio@localhost.localdomain>
2021-10-13 13:40:46 +0200

    client-grigo

--- saleor/plugins/grigoprint/accountGrigo/graphql/schema.py
+++ saleor/plugins/grigoprint/accountGrigo/graphql/schema.py
@@ -1,4 +1,11 @@
 import graphene
+from .....core.exceptions import PermissionDenied
+from .....core.permissions import AccountPermissions
+from .....core.tracing import traced_resolver
+from .....graphql.core.utils import from_global_id_or_error
+from .....graphql.utils import get_user_or_app_from_context
+
+from .....graphql.core.validators import validate_one_of_args_is_in_query
 from .....graphql.account.schema import CustomerFilterInput
 from .....graphql.account.sorters import UserSortingInput
 
@@ -33,3 +40,26 @@ class AccountQueries(graphene.ObjectType):
     #@permission_required(AccountPermissions.MANAGE_USERS)
     def resolve_customers_grigo(self, _info, **kwargs):
         return models.UserGrigo.objects.all()
+
+    def resolve_client_grigo(self, info, id=None, email=None):
+        validate_one_of_args_is_in_query("id", id, "email", email)
+        return resolve_user(info, id, email)
+
+    @traced_resolver
+    def resolve_user(info, id=None, email=None):
+        requester = get_user_or_app_from_context(info.context)
+        if requester:
+            filter_kwargs = {}
+            if id:
+                _model, filter_kwargs["pk"] = from_global_id_or_error(id, type.UserGrigo)
+            if email:
+                filter_kwargs["email"] = email
+            if requester.has_perms(
+                [AccountPermissions.MANAGE_STAFF, AccountPermissions.MANAGE_USERS]
+            ):
+                return models.UserGrigo.objects.filter(**filter_kwargs).first()
+            if requester.has_perm(AccountPermissions.MANAGE_STAFF):
+                return models.UserGrigo.objects.staff().filter(**filter_kwargs).first()
+            if requester.has_perm(AccountPermissions.MANAGE_USERS):
+                return models.UserGrigo.objects.customers().filter(**filter_kwargs).first()
+        return PermissionDenied()
